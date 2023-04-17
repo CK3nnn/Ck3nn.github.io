1c6644923d451fe549a4340dc8572d4b25ead570
CK3nnn <127086478+CK3nnn@users.noreply.github.com>
2023-04-18 01:01:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+        const toggleBtn = document.querySelector('.toggle_btn')
+        const toggleBtnIcon = document.querySelector('.toggle_btn i')
+        const dropDownMenu = document.querySelector('.dropdown_menu')
+
+        toggleBtn.onclick = function () {
+            dropDownMenu.classList.toggle('open')
+            const isOpen = dropDownMenu.classList.contains('open')
+
+            toggleBtnIcon.classList = isOpen
+                ? 'fa-solid fa-xmark'
+                : 'fa-solid fa-bars'
+        }
+
+const sr = ScrollReveal ({
+    distance: '50px',
+    duration: 2600,
+    reset: true
+})
+
+sr.reveal('.content',{delay:300, origin:'left'})
+
+
